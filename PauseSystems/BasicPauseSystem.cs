be5e6d8ea2a68ae22b83e1c7e2800cb37292d3a8
AdanVlpz7 <46058983+AdanVlpz7@users.noreply.github.com>
2021-03-13 11:05:26 -0600

    Basic Pause without calling UI
    This is just a simple method to implement a pause with just TimeScale = 0

---
+++ PauseSystems/BasicPauseSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPauseSystem : MonoBehaviour
+{
+    // This script can be add to our UIManager
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Here we should call to show a simple pause menu
+            Time.timeScale = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 1;
+        }
+
+    }
+}
