d08fcb215a4babe3e6f13bbd7649cd5283b9fa9c
AdanVlpz7 <46058983+AdanVlpz7@users.noreply.github.com>
2021-03-22 19:45:58 -0600

    An inventory script
    This script is a simple template of how we can start defining an inventory of items.

---
+++ ClassCourse/InventoryTemplate/ItemData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this script is attach to an empty object that we can rename NameDatabaseController
+[System.Serializable]
+public class Item
+{
+    public string name;
+    public int id;
+    public string description;
+
+    public Item(string name, int id, string description)
+    {
+        this.name = name;
+        this.id = id;
+        this.description = description;
+    }
+}
+public class ItemData : MonoBehaviour
+{
+    public Item[] inventory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
