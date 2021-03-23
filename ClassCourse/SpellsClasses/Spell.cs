5549600492088c58ad022ea00362e68139dfa728
AdanVlpz7 <46058983+AdanVlpz7@users.noreply.github.com>
2021-03-23 12:10:22 -0600

    Wizard doing spells
    By these two scripts we have a "Wizard" that is able to execute spells which are defined in their on class. This spells increase our exp. and also we can define their propierties on the inspector.

---
+++ ClassCourse/SpellsClasses/Spell.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Author : Adan Vargas + Jonathan Weinberger
+ * Date : 23/MAR/2021
+ * 
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Spell
+{
+    public string name;
+    public int levelRequired;
+    public int itemIdRequired;
+    public int expGained;
+
+    public Spell(string name, int levelRequired, int itemIdRequired, int exp)
+    {
+        this.name = name;
+        this.levelRequired = levelRequired;
+        this.itemIdRequired = itemIdRequired;
+        this.expGained = exp;
+    }
+
+    public void Cast()
+    {
+        Debug.Log("Casting: " + this.name);
+    }
+}
---
+++ ClassCourse/SpellsClasses/Wizard.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Author : Adan Vargas + Jonathan Weinberger
+ * Date : 23/MAR/2021
+ * 
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Wizard : MonoBehaviour
+{
+    public Spell[] spells;
+    public int level = 1;
+    public int exp;
+
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // we can iterate through the array to know which spell is of my level.
+            foreach(var spell in spells)
+            {
+                if(spell.levelRequired == level)
+                {
+                    spell.Cast();
+                    exp += spell.expGained;
+                }
+            }
+        }
+    }
+}
