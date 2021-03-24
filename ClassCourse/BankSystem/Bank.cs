390c9d323b43e161140856f973f742c574c98452
AdanVlpz7 <46058983+AdanVlpz7@users.noreply.github.com>
2021-03-24 11:08:26 -0600

    Just another example of classes inheritence

---
+++ ClassCourse/BankSystem/Bank.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Bank
+{
+    //protected works like private, but allows classes who inherites Bank to use this variables
+    protected string branchName;
+    protected string location;
+    protected int cashInVault;
+
+    protected void checkBalance()
+    {
+        Debug.Log("Checking balance " + branchName);
+    }
+    protected void Deposit()
+    {
+        Debug.Log("Depositing money to: " + branchName);
+    }
+    protected void Withdrawl()
+    {
+        Debug.Log("Withdrawl money from: " + branchName);
+    }
+}
---
+++ ClassCourse/BankSystem/FederalCreditUnions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class FederalCreditUnions : Bank
+{
+    public int availableCashToLend;
+    public void ApproveLending()
+    {
+
+    }
+}
