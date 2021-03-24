602b11ddbf58cd0f9d7549bae80d90d6321d0f17
AdanVlpz7 <46058983+AdanVlpz7@users.noreply.github.com>
2021-03-24 11:25:13 -0600

    A simple example of overriding

---
+++ ClassCourse/VirtualMethods-Overriding/Pet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pet : MonoBehaviour
+{
+    protected string name;
+    // Start is called before the first frame update
+    protected virtual void Speak()
+    {
+        Debug.Log("Speak");
+    }
+    private void Start()
+    {
+        Speak();
+        //this will print the Speak() from Dog and Duck
+    }
+}
