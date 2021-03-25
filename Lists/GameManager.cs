bd072a26ff0362ad96770f37529b166508138599
AdanVlpz7 <46058983+AdanVlpz7@users.noreply.github.com>
2021-03-25 11:15:06 -0600

    A basic list.
    Using a list an deleting randomly an element.

---
+++ Lists/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<string> names = new List<string>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        names.Add("Adan");
+        names.Add("Miguel");
+        names.Add("Nati");
+        names.Add("Sofia");
+        names.Add("Frida");
+        foreach (var name in names)
+        {
+            Debug.Log(name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            names.RemoveAt(Random.Range(0, names.Count));
+            foreach(var name in names)
+            {
+                Debug.Log(name);
+            }
+        }
+    }
+}
