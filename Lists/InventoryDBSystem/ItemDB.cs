9b7bb41db282b3b05431aec1f631204a31a597a0
AdanVlpz7 <46058983+AdanVlpz7@users.noreply.github.com>
2021-03-25 13:01:16 -0600

    Basic Inventory
    This is the beginning of setting a DB of items so our player use it's inventory.

---
+++ Lists/InventoryDBSystem/ItemDB.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    public string name;
+    public int id;
+    public Sprite icon;
+}
+
+
+public class ItemDB : MonoBehaviour
+{
+    public List<Item> ItemDataBase = new List<Item>();
+
+    public void AddItem(int itemID, Player player)
+    {
+        //check if item matches something
+        foreach(var item in ItemDataBase)
+        {
+            if(item.id == itemID)
+            {
+                Debug.Log("We have a match");
+                //we should also will know if we have "space"
+                //we will add the item to the player inventory
+                player.inventory[0] = item;
+                return;
+            }
+            else
+            {
+                Debug.Log("Item does not exist");
+            }
+        }
+    }    
+    public void RemoveItem(int itemID, Player player)
+    {
+        //check if item matches something
+        foreach (var item in ItemDataBase)
+        {
+            if (item.id == itemID)
+            {
+                Debug.Log("We have a match");
+                //we should also will know if we have "space"
+                //we will add the item to the player inventory
+                player.inventory[0] = null;
+                return;
+            }
+            else
+            {
+                Debug.Log("Item does not exist");
+            }
+        }
+    }
+}
---
+++ Lists/InventoryDBSystem/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Item[] inventory = new Item[10];
+
+    private ItemDB _itemDatabase;
+
+    private void Start()
+    {
+        inventory = new Item[10];
+        _itemDatabase = GameObject.Find("ItemDB").GetComponent<ItemDB>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //request Item by Id
+            _itemDatabase.AddItem(0,this);
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            //remove Item by Id
+            _itemDatabase.RemoveItem(0,this);
+        }
+    }
+}
