7ed208e165c4eb4e0c52b8644d04f219a540ed7e
Miguel Adán Vargas López <46058983+AdanVlpz7@users.noreply.github.com>
2021-03-29 11:42:21 -0600

    Casting enum
    Just an example using enum and calling an scene casting it's value.

---
+++ Enums/CastingEnum.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CastingEnum : MonoBehaviour
+{
+    public enum Difficulty
+    {
+        easy,
+        normal,
+        difficult
+    }
+
+    public Difficulty sceneDifficulty;
+
+    private void Start()
+    {
+        SceneManager.LoadScene((int)sceneDifficulty);
+    }
+}
