9c08cd50c144482f92b2df0ca12a5ebf23bdb73e
Miguel Adán Vargas López <46058983+AdanVlpz7@users.noreply.github.com>
2021-03-31 11:59:58 -0600

    Event example
    Those script are able to connect to a Buttom of our UI and change the color of many cubes in the scene.

---
+++ Delegates/Events/Template/Cube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this is more effective than calling each cube and turning them red.
+        //and the camera doesnt know about the cubes and the cubes about the camera.
+        //with events only the classes can subscribe over them.
+        Main.onClick += TurnRed;
+    }
+
+    public void TurnRed()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    private void OnDisable()
+    {
+        Main.onClick -= TurnRed;
+    }
+}
---
+++ Delegates/Events/Template/Main.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public delegate void ActionClick();
+    public static event ActionClick onClick;
+    public void ButtonClick()
+    {
+        //turn al cubes red
+
+        if(onClick != null)
+            onClick();
+    }
+}
