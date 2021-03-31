473a715b1e05799bfec3c9aa7345f3c6acd7a43c
Miguel Adán Vargas López <46058983+AdanVlpz7@users.noreply.github.com>
2021-03-31 12:52:09 -0600

    Teleporting Objects.
    We use those scripts to change the position of any object with the Cube.cs We use delegates and events.

---
+++ Delegates/Events/TeleportCube/Cube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this is more effective than calling each cube and turning them red.
+        //and the camera doesnt know about the cubes and the cubes about the camera.
+        //with events only the classes can subscribe over them.
+        Main.onClick += Teleport;
+    }
+
+
+    public void Teleport()
+    {
+        Debug.Log("Teleporting");
+        transform.position = new Vector3(5, 2, 0);
+    }
+
+    private void OnDisable()
+    {
+        Main.onClick -= Teleport;
+    }
+}
---
+++ Delegates/Events/TeleportCube/Main.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public delegate void ActionClick();
+    public static event ActionClick onClick;
+
+    public void HClick()
+    {
+        if (onClick != null)
+            onClick();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HClick();
+        }
+    }
+}
