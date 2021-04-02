e0f8adaa993c297240c0626861d415b41f37ec4d
Miguel Adán Vargas López <46058983+AdanVlpz7@users.noreply.github.com>
2021-04-01 20:00:47 -0600

    CallBack System
    Coroutines used

---
+++ SimpleCallBackSystem/Main.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MyRoutine(() =>
+        {
+            Debug.Log("Finished");
+        }));
+    }
+    //the null give us option to add it.
+    public IEnumerator MyRoutine(Action onComplete = null)
+    {
+        yield return new WaitForSeconds(5.0f);
+        if (onComplete != null)
+            onComplete();
+        //Debug.Log("rutine finished");
+    }
+}
