78f651c36db46f788b623b7630b3ccb20fddc9c9
Miguel Adán Vargas López <46058983+AdanVlpz7@users.noreply.github.com>
2021-04-01 19:25:56 -0600

    Action example.
    Working with Actions comparing it with delegates

---
+++ Delegates/Actions/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System; //needed for actions.
+public class Player : MonoBehaviour
+{
+    //using actions
+
+    public static Action<int> OnDamageReceived;
+    
+   // using delegates
+
+    // public delegate void OnDamageReceived(int currentHealth);
+    // public static event OnDamageReceived onDamageReceived;
+    
+    public int Health { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = 10;
+    }
+
+    void Damage()
+    {
+        Health--;
+        //not the best option.
+        // UIManager.UpdateHealth(Health);
+        
+        if(OnDamageReceived != null)
+        {
+            OnDamageReceived(Health);
+        }
+    }
+}
---
+++ Delegates/Actions/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UIManager : MonoBehaviour
+{
+    public void OnEnable()
+    {
+        Player.onDamageReceived += UpdateHealth;
+    }
+    public void UpdateHealth(int Health)
+    {
+        Debug.Log("Current Health: " + Health);
+    }
+}
